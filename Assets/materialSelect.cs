0e0d207af1df5408cc4b9b48f708a88e82eb6388
(유수빈) <(20201003@sungshin.ac.kr)>
2020-07-20 02:48:13 +0900

    7/19 유니티 기능 추가 (Play 씬에서 재료 버튼을 누르면 그 재료 설명이 뜨는 기능 추가함)

---
+++ Assets/materialSelect.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public enum MaterialType
+{
+    orangeJuice,
+    vodka,
+    rime
+
+}
+
+public class materialSelect : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public Outline buttonOutline;
+    public Text buttonText;
+    public MaterialType currentMaterial;
+
+    public void onMaterialClick()
+    {
+        Debug.Log("오렌지 주스를 클릭하셨습니까?");
+        //tiltMaterial();
+
+    }
+
+    public void tiltMaterial()
+    {
+        StartCoroutine(startTilt());
+    }
+
+    IEnumerator startTilt()
+    {
+        Debug.Log("Hello");
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        buttonOutline=GetComponent<Outline>();
+        buttonOutline.enabled = true;
+        buttonText.text = "orange juice";
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        buttonOutline = GetComponent<Outline>();
+        buttonOutline.enabled = false;
+        buttonText.text = "";
+    }
+}
